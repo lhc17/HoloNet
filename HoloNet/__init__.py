4fda196d516ce647789f60891370482d9faaf81f
lhc17 <44468696+lhc17@users.noreply.github.com>
2022-06-08 18:05:56 +0800

    Update __init__.py

--- HoloNet/__init__.py
+++ HoloNet/__init__.py
@@ -1,3 +1,5 @@
+'''HoloNet: Decoding functional cell–cell communication events by multi-view graph learning on spatial transcriptomics'''
+
 from . import plotting as pl
 from . import predicting as pr
 from . import preprocessing as pp
